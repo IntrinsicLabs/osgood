9d0b279fdef4ac928039355975275f63a2968631
Thomas Hunter II <tom@intrinsic.com>
2019-07-02 17:40:30 -0700

    build/docs: cargo build now performs npm install, fixing up docs

--- build.rs
+++ build.rs
@@ -7,8 +7,20 @@ use std::ffi::OsStr;
 use std::fs;
 use std::io::{BufWriter, Write};
 use std::path::Path;
+use std::process::Command;
 
 fn main() {
+    let target_dir = env::var("OUT_DIR").unwrap();
+    if is_lock_newer_than_binary(target_dir) {
+        println!("cargo:rerun-if-changed={}", "js/package-lock.json");
+        let child = Command::new("npm")
+            .arg("install")
+            .current_dir("js")
+            .status()
+            .unwrap();
+        assert!(child.success());
+    }
+
     let path = Path::new(&env::var("OUT_DIR").unwrap()).join("bootstrap.rs");
     let mut file = BufWriter::new(fs::File::create(&path).unwrap());
 
@@ -61,4 +73,30 @@ fn main() {
 
     map.build(&mut file).unwrap();
     write!(&mut file, ";\n").unwrap();
+}
+
+// This prevents `cargo build` from always running `npm install`
+fn is_lock_newer_than_binary(target_dir: String) -> bool {
+    let lock_file = fs::metadata("js/package-lock.json");
+    if let Err(_err) = lock_file {
+        return true;
+    }
+    let lock_file = lock_file.unwrap();
+
+    let binary_file = fs::metadata(format!("{}/osgood", target_dir));
+    // let binary_file = fs::metadata("target/debug/osgood");
+    if let Err(_err) = binary_file {
+        return true;
+    }
+    let binary_file = binary_file.unwrap();
+
+    if let Ok(lock_time) = lock_file.modified() {
+        if let Ok(binary_time) = binary_file.modified() {
+            return lock_time > binary_time;
+        } else {
+            return true;
+        }
+    } else {
+        return true;
+    }
 }
